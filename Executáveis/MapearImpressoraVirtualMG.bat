1d8321f5937996cec05f45680d21cd173f60dff9
metallixsmsgroup <daniel.almeida@sms-group.com>
2023-05-18 14:03:41 -0300

    Mudança de pasta/deletados

--- Executáveis/MapearImpressoraVirtualMG.bat
+++
@@ -1,7 +0,0 @@
-echo off
-echo Instalando impressora virtual. VespasianoMG
-
-rundll32 printui.dll PrintUIEntry /in /n \\SVVSAIMP02\IMPRESSORA_VIRTUAL
-
-echo off
-echo Impressora instalada com sucesso. VespasianoMG
--- Executáveis/mapTeamFolder.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host 'Forneca as informacoes abaixo para mapear as suas Team Folders:'
-$net = new-object -ComObject WScript.Network
-$user = Read-Host -Prompt 'Digite seu usuario SMS (seu usuario SMS tem 6 digitos):'
-$password = Read-Host -Prompt 'Digite sua senha:'
-$net.MapNetworkDrive("t:", "\\sms-group\teamfolder", $true, "sms-group\$user", "$password")
-Write-Host 'TeamFolder mapeada !Voce pode acessa-la no disco T:'
-pause
